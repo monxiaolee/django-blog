6b38d25c5cd7f81dea15da25626ee4c9aece32b6
limengxiao <15893623902Lee>
2018-06-08 17:40:00 +0800

    提交django-blog

---
+++ blog/views.py
@@ -0,0 +1,45 @@
+# -*- coding: UTF-8 -*-
+from django.shortcuts import render
+
+# Create your views here.
+
+from comments.forms import CommentForm
+from django.http import HttpResponse
+import markdown
+from django.shortcuts import render, get_object_or_404
+from . models import Post, Category
+
+def index(request):
+    Post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': Post_list})
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # 记得在顶部引入 markdown 模块
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    # 记得在顶部导入 CommentForm
+    form = CommentForm()
+    # 获取这篇 post 下的全部评论
+    comment_list = post.comment_set.all()
+
+    # 将文章、表单、以及文章下的评论列表作为模板变量传给 detail.html 模板，以便渲染相应数据。
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context)
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year, created_time__month=month).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
